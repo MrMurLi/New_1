24a7684ff7c5ac8f56e281948d0506181f62377c
MrMurLi <123119672+MrMurLi@users.noreply.github.com>
2023-01-20 12:47:14 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,3 +1,11 @@
-a = [1, 10, 2, 9]
-m = sorted(a)[len(a) // 2]
-print(sum(abs(v - m) for v in a))
+count = 0
+nums_result = [int(i) for i in nums]
+    result_digit = sum(nums_result) // len(nums_result)
+    for item in nums_result:
+        if item < result_digit:
+            count += (result_digit - item)
+        elif item > result_digit:
+            count += item - result_digit
+        else:
+            pass
+    print(count)
