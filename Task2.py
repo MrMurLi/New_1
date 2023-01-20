222ba89f92791ecff8d9f069522b0e0e7e4b7ffd
MrMurLi <123119672+MrMurLi@users.noreply.github.com>
2023-01-20 11:01:57 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -5,7 +5,7 @@ x_point = float(input("x = "))
 y_point = float(input("y = "))
 r_circle = float(input("R = "))
  
-hypotenuse = math.sqrt(x_point  2 + y_point  2)
+hypotenuse = math.sqrt(x_point**2 + y_point**2)
  
 if hypotenuse <= r_circle:
     print("1")
